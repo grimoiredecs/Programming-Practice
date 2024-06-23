95c65b9ad9c5160c10689ed859ac2bbf38a1e355
grimoiredecs <khangtien24@gmail.com>
2024-06-23 23:39:33 +0700

    first

---
+++ Leetcode/Arrays/Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*
+ *
+ * Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+*/
+int longestSubarrayK(vector<int>& nums, int limit, int K) 
+{
+        int n = nums.size();
+    for(int i =0; i < n-K+1; ++i)
+  {
+    
+  }
+}
+
+
+
+int main()
+{
+  return 0;
+}
