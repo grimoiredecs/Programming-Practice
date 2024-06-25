ad815714e45760046c1275321df206d96c8ed96d
grimoiredecs <khangtien24@gmail.com>
2024-06-25 16:43:59 +0700

    halfway through

---
+++ CSES/Introductory/Missing_Number/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int missing_number(int n, vector<int> arr){
+    int sum2=0;
+    int size = arr.size();
+    int sum1=0;
+    for(int i =0; i < size;i++){
+        sum1 += arr[i];
+    }
+    for()
+}
+
+int main(){
+
+}
