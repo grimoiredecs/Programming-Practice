67afd0d72c2d0bfcf50c51d347f304e18f731b5a
116292903+grimoiredecs@users.noreply.github.com <116292903+grimoiredecs@users.noreply.github.com>
2024-09-22 21:20:26 +0700

    finishing happy number

--- Leetcode/Happy_Number/main.py
+++ Leetcode/Happy_Number/main.py
@@ -1,5 +1,17 @@
-def isHappy(n:int) -> bool:
-    numbers = []
-    for i in range(len(numbers)):
-        
+def isHappy(n:int) -> list:
+    seen = set()
+    while n!=1:
+        if n  in seen:
+            return False
+        seen.add(n)
+        total =0
+        while n>0:
+            digit = n%10
+            total += digit*digit
+            n = n//10
+        n = total
+    return True
+            
     
+numm = 19
+print(isHappy(numm))
