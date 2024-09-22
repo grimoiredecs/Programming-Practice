3a4dc97bd2dc2169c433cef4eba5d5c330fdf57a
116292903+grimoiredecs@users.noreply.github.com <116292903+grimoiredecs@users.noreply.github.com>
2024-09-22 20:54:25 +0700

    added happy number

---
+++ Leetcode/Happy_Number/main.py
@@ -0,0 +1,5 @@
+def isHappy(n:int) -> bool:
+    numbers = []
+    for i in range(len(numbers)):
+        
+    
---
+++ Network/java/dayone.java
@@ -0,0 +1,8 @@
+import java.net*;
+class dayone
+{
+    public static void main(String args[]) throws UnknownHostException{
+        InetAddress Address = InetAddress.getLocalHost();
+
+    }
+}
